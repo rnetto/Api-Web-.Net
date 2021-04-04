14b0da5f5095d79651baf1af6405bc85fba0a055
Renato Netto <rnetto94@live.com>
2021-04-04 11:40:13 -0300

    Api funcionando adc, altera, deleta funcoes

--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
 
 namespace TodoApi
 {
@@ -26,7 +28,9 @@ namespace TodoApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            // add comando para starta o BD;
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+            
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
